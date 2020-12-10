4a293dec2fc1bc3354d6af25799ae3d22523750a
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2020-12-10 13:36:47 +0000

    📦 Release v1.0.2 plerr

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='plerr',
-    version='1.0.1',
+    version='1.0.2',
     author='Vladyslav Krylasov',
     author_email='vladyslav.krylasov@gmail.com',
     description=(
