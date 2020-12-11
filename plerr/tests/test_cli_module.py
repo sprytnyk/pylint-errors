3cb58d86a292828daa3a3c6b7cabd12865ebf398
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2020-12-11 16:49:47 +0000

    ✅ Add tests to cover a new search scenario

--- plerr/tests/test_cli_module.py
+++ plerr/tests/test_cli_module.py
@@ -1,4 +1,5 @@
 """Tests CLI modules to provide factual coverage."""
+
 import io
 import pathlib
 import sys
@@ -7,10 +8,12 @@ from unittest.mock import patch
 
 from plerr.cli import main
 
+ROOT = pathlib.Path(__file__).resolve().parent
+
 
 class TestPlErrModule(unittest.TestCase):
     def setUp(self):
-        # Redirect print output to an objects to read later
+        # Redirect stdout/stderr to mock objects to read later.
         self.stdout = patch('plerr.cli.sys.stdout', new=io.StringIO())
         self.stderr = patch('plerr.cli.sys.stderr', new=io.StringIO())
         self.stdout.start()
@@ -20,21 +23,31 @@ class TestPlErrModule(unittest.TestCase):
         self.stdout.stop()
         self.stderr.stop()
 
-    @patch('plerr.cli.sys.argv', new=['plerr', 'R1710'])
-    def test_plerr_error_getter(self):
+    @patch('plerr.cli.sys.argv', new=['plerr', 'r1710'])
+    def test_plerr_error_getter_with_lower_case_letter(self):
         # Given: a command to get a description of a pylint error by an
-        # error code.
+        # error code with a lower case letter.
 
         # When: the command invokes.
         with self.assertRaises(SystemExit) as err:
             main()
-        expected_stdout = (
-            (
-                pathlib.Path(__file__).resolve().parent /
-                'command_output_fixture.txt'
-            )
-            .read_bytes()
-        )
+        expected_stdout = (ROOT / 'command_output_fixture.txt').read_bytes()
+
+        # Then: it produces a highlighted output to stdout of the given error
+        # with the exit code 0.
+        assert sys.stdout.getvalue().encode() == expected_stdout
+        assert not sys.stderr.getvalue().encode()
+        assert err.exception.code == 0
+
+    @patch('plerr.cli.sys.argv', new=['plerr', 'R1710'])
+    def test_plerr_error_getter_with_capital_letter(self):
+        # Given: a command to get a description of a pylint error by an
+        # error code with a capital letter.
+
+        # When: the command invokes.
+        with self.assertRaises(SystemExit) as err:
+            main()
+        expected_stdout = (ROOT / 'command_output_fixture.txt').read_bytes()
 
         # Then: it produces a highlighted output to stdout of the given error
         # with the exit code 0.
@@ -44,17 +57,17 @@ class TestPlErrModule(unittest.TestCase):
 
     @patch('plerr.cli.sys.argv', new=['plerr', 'R0000'])
     def test_plerr_non_existent_error(self):
-        # Given: a command to get a description of a pylint error with an
-        # existent error code.
+        # Given: a command to get a description of a pylint error with a
+        # non-existent error code.
 
         # When: the command invokes.
         with self.assertRaises(SystemExit) as err:
             main()
-        expected_stdout = (
+        expected_stderr = (
             b'Cannot find R0000 pylint error by such error code.\n'
         )
 
         # Then: it produces an error message to stderr with the exit code 1.
-        assert sys.stderr.getvalue().encode() == expected_stdout
+        assert sys.stderr.getvalue().encode() == expected_stderr
         assert not sys.stdout.getvalue().encode()
         assert err.exception.code == 1
--- plerr/tests/test_package.py
+++ plerr/tests/test_package.py
@@ -1,13 +1,36 @@
 """Tests a package installation on a user OS."""
+
 import pathlib
 import subprocess
 import unittest
 
+ROOT = pathlib.Path(__file__).resolve().parent
+
 
 class TestPlErrPackage(unittest.TestCase):
-    def test_plerr_error_getter(self):
+    def test_plerr_error_getter_with_lower_case_letter(self):
         # Given: a command to get a description of a pylint error by an
-        # error code.
+        # error code with a lower case letter.
+        command = ['python3', '-m', 'plerr', 'r1710']
+
+        # When: the command invokes.
+        proc = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        stdout, stderr = proc.communicate()
+        expected_stdout = (ROOT / 'command_output_fixture.txt').read_bytes()
+
+        # Then: it produces a highlighted output to stdout of the given error
+        # with the exit code 0.
+        assert stdout == expected_stdout
+        assert not stderr
+        assert proc.returncode == 0
+
+    def test_plerr_error_getter_with_capital_letter(self):
+        # Given: a command to get a description of a pylint error by an
+        # error code with a capital letter.
         command = ['python3', '-m', 'plerr', 'R1710']
 
         # When: the command invokes.
@@ -17,13 +40,7 @@ class TestPlErrPackage(unittest.TestCase):
             stderr=subprocess.PIPE
         )
         stdout, stderr = proc.communicate()
-        expected_stdout = (
-            (
-                pathlib.Path(__file__).resolve().parent /
-                'command_output_fixture.txt'
-            )
-            .read_bytes()
-        )
+        expected_stdout = (ROOT / 'command_output_fixture.txt').read_bytes()
 
         # Then: it produces a highlighted output to stdout of the given error
         # with the exit code 0.
@@ -32,8 +49,8 @@ class TestPlErrPackage(unittest.TestCase):
         assert proc.returncode == 0
 
     def test_plerr_non_existent_error(self):
-        # Given: a command to get a description of a pylint error with an
-        # existent error code.
+        # Given: a command to get a description of a pylint error with a
+        # non-existent error code.
         command = ['python3', '-m', 'plerr', 'R0000']
 
         # When: the command invokes.
@@ -43,11 +60,11 @@ class TestPlErrPackage(unittest.TestCase):
             stderr=subprocess.PIPE
         )
         stdout, stderr = proc.communicate()
-        expected_stdout = (
+        expected_stderr = (
             b'Cannot find R0000 pylint error by such error code.\n'
         )
 
         # Then: it produces an error message to stderr with the exit code 1.
-        assert stderr == expected_stdout
+        assert stderr == expected_stderr
         assert not stdout
         assert proc.returncode == 1
